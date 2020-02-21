12275655976e227f49978be2089dd37cadef04ee
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2020-02-20 22:51:29 -0300

    script e css implementados

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-let tempoDiario = document.getElementById("diario").value;
+document.getElementById("resultado").style.visibility = 'hidden';
+function calcularValor() {
+    let horasDiarias = document.getElementById("diario").value;
+    let diasEfetivos = document.getElementById("efetivos").value;
+    let diasFerias = document.getElementById("ferias").value;
+    let valorProjeto = document.getElementById("valor").value;
 
-calcularValor = function () {
-    alert(tempoDiario);
+    valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias) ) + ( ( diasFerias * diasEfetivos * horasDiarias ) )
+
+    document.querySelector("#resultado h1").innerHTML = "R$ " + valorHora;
+    console.log(valorHora);
+    
+    document.getElementById("resultado").style.visibility = 'inherit';
 }
+
