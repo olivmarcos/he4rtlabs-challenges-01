e6a956c08f3b55486ef15f1f7713f19cdf7a716a
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2020-02-26 16:21:55 -0300

    validacoes funcionando

--- src/main.js
+++ src/main.js
@@ -1,15 +1,63 @@
 document.getElementById("resultado").style.visibility = 'hidden';
+
 function calcularValor() {
-    let horasDiarias = document.getElementById("diario").value;
-    let diasEfetivos = document.getElementById("efetivos").value;
-    let diasFerias = document.getElementById("ferias").value;
-    let valorProjeto = document.getElementById("valor").value;
 
-    valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias) ) + ( ( diasFerias * diasEfetivos * horasDiarias ) )
+    let valorEleemtos = pegarElementos();
 
-    document.querySelector("#resultado h1").innerHTML = "R$ " + valorHora;
-    console.log(valorHora);
-    
-    document.getElementById("resultado").style.visibility = 'inherit';
+    valorProjeto = valorEleemtos[3].value;
+    diasEfetivos = valorEleemtos[1].value;
+    horasDiarias = valorEleemtos[0].value;
+    diasFerias = valorEleemtos[2].value;
+
+    validarForm(horasDiarias, diasEfetivos, diasFerias, valorProjeto);
+
+    valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias)) + ((diasFerias * diasEfetivos * horasDiarias))
+
+    if (valorHora) {
+        document.querySelector("#resultado h1").innerHTML = "R$ " + valorHora;
+        console.log(valorHora);
+
+        document.getElementById("resultado").style.visibility = 'inherit';
+    }
 }
 
+function validarForm(horasDiarias, diasEfetivos, diasFerias, valorProjeto) {
+    let elementos = pegarElementos();
+
+    if (isNaN(horasDiarias)) {
+        elementos[0].classList.add("warn");
+        setTimeout(() => {
+            elementos[0].classList.remove("warn");
+        }, 500);
+    }
+
+    if (isNaN(diasEfetivos)) {
+        elementos[1].classList.add("warn");
+        setTimeout(() => {
+            elementos[1].classList.remove("warn");
+        }, 500);
+    }
+
+    if (isNaN(diasFerias)) {
+        elementos[2].classList.add("warn");
+        setTimeout(() => {
+            elementos[2].classList.remove("warn");
+        }, 500);
+    }
+
+    if (isNaN(valorProjeto)) {
+        elementos[3].classList.add("warn");
+        setTimeout(() => {
+            elementos[3].classList.remove("warn");
+        }, 500);
+    }
+}
+
+function pegarElementos() {
+    let horasDiarias = document.getElementById("diario");
+    let diasEfetivos = document.getElementById("efetivos");
+    let diasFerias = document.getElementById("ferias");
+    let valorProjeto = document.getElementById("valor");
+
+    return elementos = [horasDiarias, diasEfetivos, diasFerias, valorProjeto];
+}
